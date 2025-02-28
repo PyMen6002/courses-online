601a43e2b6193b0ed1f75fecdfbf1132f06feb94
PyMen <92265050+PyMen6002@users.noreply.github.com>
2025-02-28 16:59:11 +0100

    Configuración de STATIC_ROOT en settings.py y archivos estáticos

--- cursos_online/urls.py
+++ cursos_online/urls.py
@@ -1,8 +1,13 @@
 # cursos_online/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('cursos.urls')),  # Esto incluirá las URLs de la app cursos
 ]
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
